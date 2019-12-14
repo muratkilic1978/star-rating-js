d31559af5837b5f3770dc13cebb3a628136b2ba3
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-14 14:57:42 +0100

    script.js - function starRate was modified

--- js/script.js
+++ js/script.js
@@ -1,34 +1,36 @@
 const stars = document.querySelectorAll(".star");
 const output = document.querySelector(".output");
 
+// for loop that iterates through all stars
 for(let x = 0; x < stars.length; x++){
+    // In order to distinguish between the different stars, we assign them a unique number
     stars[x].starValue = (x+1);
     console.log(stars[x]);
     //stars[x].addEventListener("click",starRate);
-    ["mouseover","mouseout","click"].forEach(function(e){
+    ["click"].forEach(function(e){
         stars[x].addEventListener(e, starRate);  
     })
 }
 
 
 function starRate(e){
+    //eventValue is keeping track of the eventType eg. click, mouseover mouseout 
     let eventValue = e.type;
+    //console.log(eventValue);
+    // starValue is keeping track of how many stars you have clicked on
     let starValue = this.starValue;
-    if(eventValue === "click"){
-        if(starValue > 0){
-            output.innerHTML = "You rated this "+starValue+ " stars.";
+    //console.log(starValue);
+    stars.forEach(function(elem,index){
+        console.log(elem);
+        console.log(index);
+        if(index < starValue){
+            elem.classList.add("gold");
+            
+        }else{
+            elem.classList.remove("gold");
         }
+        output.innerHTML = "You rated this "+starValue+ " stars";
+    })
     }
     
     
-    stars.forEach(function(ele,ind){
-        if(eventValue === "click"){
-            if(ind < starValue) {
-                ele.classList.add("gold");
-            }else{
-                ele.classList.remove("gold");
-            }
-        }
-        
-    })
-}
