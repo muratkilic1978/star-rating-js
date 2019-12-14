374803502abb40dcccaac3ec46c05c98f7134591
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-14 07:36:27 +0100

    script.js was updated

--- js/script.js
+++ js/script.js
@@ -4,10 +4,14 @@ const output = document.querySelector(".output");
 for(let x = 0; x < stars.length; x++){
     stars[x].starValue = (x+1);
     console.log(stars[x]);
-    stars[x].addEventListener("click",starRate);
+    //stars[x].addEventListener("click",starRate);
+    ["mouseover","mouseout","click"].forEach(function(e){
+        stars[x].addEventListener(e, starRate);  
+    })
 }
 
 
 function starRate(e){
     console.log(this.starValue);
+    console.log(e.type);
 }
